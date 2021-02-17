b47e7b1653f4d9c50ab70a0ab70222e61a634a00
Strong developer <Strong@DESKTOP-89QCP1E>
2021-02-17 12:29:35 +0300

    Update Quilbot_Rewriter.py

--- Quilbot_Rewriter.py
+++ Quilbot_Rewriter.py
@@ -22,6 +22,27 @@ class My_App(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.parent = parent
+        options = webdriver.ChromeOptions()
+        # options.add_argument('--headless')
+        # options.add_argument('--no-sandbox')
+        # options.add_argument('--disable-dev-shm-usage')
+
+        self.driver = webdriver.Chrome(str(pathlib.Path(__file__).parent.absolute()) + '\\chromedriver.exe',options=options)
+        url="https://quillbot.com/"
+        self.driver.get(url)
+        elelogins = self.driver.find_elements_by_class_name('MuiButton-label')
+        for i in range(0, len(elelogins)):
+            if (elelogins[i].text == "Log In"):
+                elelogins[i].click()
+                break
+
+        time.sleep(2)
+
+        eleInputs = self.driver.find_elements_by_class_name('MuiFilledInput-input')
+        eleInputs[0].send_keys('fadeevdoma@gmail.com')
+        eleInputs[1].send_keys('quillbotpass')
+
+        self.driver.find_element_by_class_name('auth-btn').click()        
         
         lblInput = Label(self.parent, font="Verdana 10 bold", text = "InputText : ")
         lblInput.place(x=70, y=40)
@@ -47,41 +68,26 @@ class My_App(tk.Frame):
 
         sInput = self.txtInput.get("1.0", 'end-1c')
         sListInput = sInput.split('\n')
-        sListOutput = sListInput
+        sListOutput = sListInput       
 
-        options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
-        # options.add_argument('--no-sandbox')
-        # options.add_argument('--disable-dev-shm-usage')
+        try: 
+            if(len(self.driver.find_elements_by_class_name('MuiDialog-paper')) > 0):
+                elebuttons = self.driver.find_elements_by_class_name('MuiIconButton-root')
+                for i in range(0, len(elebuttons)):
+                    try:
+                        elebuttons[i].click()
+                        break
+                    except:
+                        pass     
 
-        driver = webdriver.Chrome(str(pathlib.Path(__file__).parent.absolute()) + '\\chromedriver.exe',options=options)
-        url="https://quillbot.com/"
-        driver.get(url)
-
-        try:
-            # elelogins = driver.find_elements_by_class_name('MuiButton-label')
-            # for i in range(0, len(elelogins)):
-            #     if (elelogins[i].text == "Log In"):
-            #         elelogins[i].click()
-            #         break
-
-            # time.sleep(2)
-
-            # eleInputs = driver.find_elements_by_class_name('MuiFilledInput-input')
-            # eleInputs[0].send_keys('fadeevdoma@gmail.com')
-            # eleInputs[1].send_keys('quillbotpass')
-
-            # driver.find_element_by_class_name('auth-btn').click()
-
-            # time.sleep(2)
-
-            eleSliders = driver.find_elements_by_class_name('MuiSlider-thumbColorPrimary')
+            eleSliders = self.driver.find_elements_by_class_name('MuiSlider-thumbColorPrimary')
 
             for i in range(0, len(eleSliders)):
                 try:
                     eleSliders[i].send_keys(Keys.ARROW_RIGHT)
                     eleSliders[i].send_keys(Keys.ARROW_RIGHT)
                     eleSliders[i].send_keys(Keys.ARROW_RIGHT)
+                    break
                 except:
                     pass
 
@@ -89,8 +95,8 @@ class My_App(tk.Frame):
 
                 if (sListInput[v] != ""):
 
-                    if(len(driver.find_elements_by_class_name('MuiDialog-paper')) > 0):
-                        elebuttons = driver.find_elements_by_class_name('MuiIconButton-root')
+                    if(len(self.driver.find_elements_by_class_name('MuiDialog-paper')) > 0):
+                        elebuttons = self.driver.find_elements_by_class_name('MuiIconButton-root')
                         for i in range(0, len(elebuttons)):
                             try:
                                 elebuttons[i].click()
@@ -98,19 +104,19 @@ class My_App(tk.Frame):
                             except:
                                 pass     
 
-                    WebDriverWait(driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0')))              
+                    WebDriverWait(self.driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0')))              
 
-                    eleInputbox = driver.find_element_by_id('inputText')
+                    eleInputbox = self.driver.find_element_by_id('inputText')
                     eleInputbox.clear()
                     eleInputbox.send_keys(sListInput[v])                
 
                     time.sleep(1)
-                    WebDriverWait(driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0'))).click()
+                    WebDriverWait(self.driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0'))).click()
                     time.sleep(1)
 
-                    WebDriverWait(driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0')))
+                    WebDriverWait(self.driver, 600).until(ec.element_to_be_clickable((By.CLASS_NAME, 'QuillButton-sc-12j9igu-0')))
 
-                    sOutput = driver.find_element_by_id('articleTextArea').text
+                    sOutput = self.driver.find_element_by_id('articleTextArea').text
                     sListOutput[v] = sOutput
 
         except Exception as e:
